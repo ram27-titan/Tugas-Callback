0c85b5e5b827daebcff74500459f4ea66f9d8b3d
Alam <76722974+ram27-titan@users.noreply.github.com>
2022-01-30 16:15:16 +0700

    Update coding javascript

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ $.ajax({
   url: 'https://jsonplaceholder.typicode.com/users',
   success: (results) => {
     const dataUser = results;
+    const street = dataUser[0].address.street;
+    const suite = dataUser[1].address.suite;
+    const city = dataUser[2].address.city;
+    const name = dataUser[0].company.name;
     let tables = '';
     dataUser.forEach((m) => {
       tables += `<tr>
@@ -9,8 +13,8 @@ $.ajax({
       <td>${m.name}</td>
       <td>${m.username}</td>
       <td>${m.email}</td>
-      <td>${m.address}</td>
-      <td>${m.company}</td>
+      <td>${m.address.street}, ${m.address.suite}, ${m.address.city}</td>
+      <td>${m.company.name}</td>
     </tr>`;
     });
     $('.table-container').html(tables);
