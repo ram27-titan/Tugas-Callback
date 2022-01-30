630ad9c2b57a958258ac50f01879a2313e1383e1
Alam <76722974+ram27-titan@users.noreply.github.com>
2022-01-30 22:37:09 +0700

    fitur loading

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-$.ajax({
-  url: 'https://jsonplaceholder.typicode.com/users',
-  success: (results) => {
-    const dataUser = results;
-    const street = dataUser[0].address.street;
-    const suite = dataUser[1].address.suite;
-    const city = dataUser[2].address.city;
-    const name = dataUser[0].company.name;
-    let tables = '';
-    dataUser.forEach((m) => {
-      tables += `<tr>
-      <th scope="row">${m.id}</th>
-      <td>${m.name}</td>
-      <td>${m.username}</td>
-      <td>${m.email}</td>
-      <td>${m.address.street}, ${m.address.suite}, ${m.address.city}</td>
-      <td>${m.company.name}</td>
-    </tr>`;
-    });
-    $('.table-container').html(tables);
-  },
-});
+$body = $("body");
+
+$body.addClass("loading");
+
+setTimeout(function() {
+  $.ajax({
+    url: 'https://jsonplaceholder.typicode.com/users',
+    success: (results) => {
+  
+      const dataUser = results;
+      const street = dataUser[0].address.street;
+      const suite = dataUser[1].address.suite;
+      const city = dataUser[2].address.city;
+      const name = dataUser[0].company.name;
+      let tables = '';
+      dataUser.forEach((m) => {
+        tables += `<tr>
+        <th scope="row">${m.id}</th>
+        <td>${m.name}</td>
+        <td>${m.username}</td>
+        <td>${m.email}</td>
+        <td>${m.address.street}, ${m.address.suite}, ${m.address.city}</td>
+        <td>${m.company.name}</td>
+      </tr>`;
+      });
+      $('.table-container').html(tables);
+      $body.removeClass("loading");
+    },
+  });  
+}, 5000);
